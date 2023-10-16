851b2110afffe9db3311d22da2c7cfa76842394c
bakayks <bakaiks312@gmail.com>
2023-10-16 16:45:30 +0600

    Initial commit

---
+++ app/src/main/java/pl/aifer/youtube_sandbox_m6_l3/presentation/playlists/PlaylistsAdapter.kt
@@ -0,0 +1,51 @@
+package pl.aifer.youtube_sandbox_m6_l3.presentation.playlists
+
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import pl.aifer.youtube_sandbox_m6_l3.data.model.PlaylistsModel
+import pl.aifer.youtube_sandbox_m6_l3.databinding.ItemPlaylistsBinding
+
+internal class PlaylistsAdapter(private val onClickItem: (playlistItem: PlaylistsModel.Item) -> Unit) :
+    RecyclerView.Adapter<PlaylistsAdapter.PlaylistsViewHolder>() {
+
+    private var _playlists = mutableListOf<PlaylistsModel.Item>()
+    private val playlists: List<PlaylistsModel.Item> get() = _playlists
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PlaylistsViewHolder {
+        return PlaylistsViewHolder(
+            ItemPlaylistsBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: PlaylistsViewHolder, position: Int) {
+        holder.bind(playlists[position])
+    }
+
+    override fun getItemCount() = playlists.size
+
+    inner class PlaylistsViewHolder(private val binding: ItemPlaylistsBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        @SuppressLint("SetTextI18n")
+        fun bind(playlist: PlaylistsModel.Item) {
+            binding.tvTitle.text = playlist.snippet.title
+            binding.tvSubtitle.text =
+                playlist.contentDetails.itemCount.toString() + " video series"
+            binding.imgPlaylists.load(playlist.snippet.thumbnails.default.url)
+            itemView.setOnClickListener { onClickItem(playlist) }
+        }
+    }
+
+    fun updateData(newPlaylists: List<PlaylistsModel.Item>) {
+        _playlists.clear()
+        _playlists.addAll(newPlaylists)
+        notifyItemRangeInserted(_playlists.size, newPlaylists.size - _playlists.size)
+    }
+}
